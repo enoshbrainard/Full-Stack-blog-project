b5593d204865635517adcf19274077f37b1a2d51
enoshbrainard <158431698+enoshbrainard@users.noreply.github.com>
2025-06-22 20:09:23 +0530

    Update middleware.js

--- src/middleware.js
+++ src/middleware.js
@@ -1,22 +1,8 @@
 import { NextResponse } from "next/server";
-import { jwtVerify } from "jose";
 
-export async function middleware(request) {
-  const token = request.cookies.get("jwt")?.value;
-
-  if (!token) {
-    console.log("⛔ No token, redirecting...");
-    // return NextResponse.redirect(new URL("/login", request.url));
-  }
-
-  try {
-    const secret = new TextEncoder().encode(process.env.JWT_SECRET);
-    await jwtVerify(token, secret); // No need to store decoded if not needed
-    return NextResponse.next();
-  } catch (err) {
-    console.log("❌ Invalid token, redirecting...");
-    // return NextResponse.redirect(new URL("/login", request.url));
-  }
+export function middleware(request) {
+  console.log("✅ Middleware is running for:", request.nextUrl.pathname);
+  return NextResponse.next();
 }
 
 export const config = {
